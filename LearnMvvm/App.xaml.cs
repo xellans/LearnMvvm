ad756335e1c294ae06e4b71f2fa1276f0ef59074
xellans <130226258+xellans@users.noreply.github.com>
2024-03-06 19:56:37 +0700

    Мелкие правки

--- LearnMvvm/App.xaml.cs
+++ LearnMvvm/App.xaml.cs
@@ -1,15 +1,64 @@
-﻿using System.Configuration;
+﻿using Common.Standard.Interfaces.Model;
+using Common.Standard.Interfaces.ViewModel;
+using DataBase;
+using Repositories;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ViewModel;
+using WpfCore;
 
 namespace LearnMvvm
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-       
+        private static ContextRepositories contextRepositories {  get; set; }
+        private static NavigatorLocator locator { get; set; }
+        private static Context context { get; set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            locator = (NavigatorLocator)this.FindResource("locator");
+            context = new Context();
+            contextRepositories = new ContextRepositories(context);
+            IAuthorized authorized = new Authorized();
+            locator.AuthVM = new AuthVM(authorized);
+        }
+        public static RoutedCommand SetCurrentContext = new RoutedCommand();
+
+        static App() => CommandManager.RegisterClassCommandBinding(typeof(UIElement), new CommandBinding(SetCurrentContext, CurrentContextCommand));
+        private static void CurrentContextCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (nameof(PersonVM).Equals(e.Parameter))
+            {
+                if (locator.PersonVM == null)
+                {
+                    IPersonModel model = new PersonModel(contextRepositories.Person());
+                    locator.PersonVM = new PersonVM(model);
+                }
+                locator.CurrentContext = locator.PersonVM;
+            }
+
+            if (nameof(ProductVM).Equals(e.Parameter))
+            {
+                if (locator.ProductVM == null)
+                {
+                    IProductModel model = new ProductModel(contextRepositories.Products());
+                    locator.ProductVM = new ProductVM(model);
+                }
+                locator.CurrentContext = locator.ProductVM;
+            }
+
+            if (nameof(SettingVM).Equals(e.Parameter))
+            {
+                if (locator.SettingVM == null)
+                    locator.SettingVM = new SettingVM();
+                locator.CurrentContext = locator.SettingVM;
+            }
+        }
+
     }
 
 }
--- LearnMvvm/NavigatorLocator.cs
+++ LearnMvvm/NavigatorLocator.cs
@@ -8,10 +8,8 @@ using ViewModel;
 
 namespace LearnMvvm
 {
-    // Это классы уровня View
     public class NavigatorLocator : ViewModelBase
     {
-        private ContextRepositories ContextRepositories { get; set; }
         public IAuthVM? AuthVM { get => Get<IAuthVM>(); set => Set(value); }
         
         public IPersonVM? PersonVM { get => Get<IPersonVM>(); set => Set(value); }
@@ -19,58 +17,14 @@ namespace LearnMvvm
         public IProductVM? ProductVM { get => Get<IProductVM>(); set => Set(value); }
         public ISettingVM? SettingVM { get => Get<SettingVM>(); set => Set(value); }
 
-
-        public NavigatorLocator()
-        {
-            Context context = new Context();
-            ContextRepositories = new ContextRepositories(context);
-            IAuthorized authorized = new Authorized();
-            AuthVM = new AuthVM(authorized);
-        }
-
         public object? CurrentContext { get => Get<object>(); set => Set(value); }
 
-        public RelayCommand SetCurrentContext => GetCommand<object?>(obj =>
-        {
-            if (nameof(PersonVM).Equals(obj))
-            {
-                IPersonModel model = new PersonModel(ContextRepositories.Person());
-                if(PersonVM == null)
-                PersonVM = new PersonVM(model);
-                obj = PersonVM;
-            }
-
-            if (nameof(ProductVM).Equals(obj))
-            {
-                IProductModel model = new ProductModel(ContextRepositories.Products());
-                if (ProductVM == null)
-                    ProductVM = new ProductVM(model);
-                obj = ProductVM;
-            }
-
-            if (nameof(SettingVM).Equals(obj))
-            {
-                if (SettingVM == null)
-                    SettingVM = new SettingVM();
-                obj = SettingVM;
-            }
-
-            CurrentContext = obj;
-        });
 
 
+        public static NavigatorLocator GetNavigator(DependencyObject obj) => (NavigatorLocator)obj.GetValue(NavigatorProperty);
 
-        public static NavigatorLocator GetNavigator(DependencyObject obj)
-        {
-            return (NavigatorLocator)obj.GetValue(NavigatorProperty);
-        }
+        public static void SetNavigator(DependencyObject obj, NavigatorLocator value) => obj.SetValue(NavigatorProperty, value);
 
-        public static void SetNavigator(DependencyObject obj, NavigatorLocator value)
-        {
-            obj.SetValue(NavigatorProperty, value);
-        }
-
-        // Using a DependencyProperty as the backing store for Navigator.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty NavigatorProperty =
             DependencyProperty.RegisterAttached("Navigator", typeof(NavigatorLocator), typeof(NavigatorLocator), new PropertyMetadata(null));
 
